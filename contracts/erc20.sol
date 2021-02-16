04c51397f80649e52205a6553a67335c6fd96a76
Matthew Watman <mattwatman@gmail.com>
2021-02-16 17:05:22 -0500

    Implemented 6  mandatory functions

--- contracts/erc20.sol
+++ contracts/erc20.sol
@@ -1,42 +1,61 @@
-pragma solidity ^0.7.0 ;
+// https://eips.ethereum.org/EIPS/eip-20
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.8.1;
 
-abstract contract ERC20Interface {
-    function totalSupply() 
-		virtual
-        public 
-		view 
-		returns (uint);
+interface Token {
 
-    function balanceOf(address tokenOwner) 
-		virtual
-        public 
-		view 
-		returns (uint balance);
-    
-	function allowance
-		(address tokenOwner, address spender) 
-		virtual
-        public 
-		view 
-		returns (uint remaining);
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return balance the balance
+    function balanceOf(address _owner) external view returns (uint256 balance);
 
-    function transfer(address to, uint tokens) virtual public 
-		returns (bool success);
-    
-	function approve(address spender, uint tokens) virtual public 
-		returns (bool success);
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return success Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) external returns (bool success);
 
-    function transferFrom 
-		(address from, address to, uint tokens) virtual public 
-		returns (bool success);
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return success Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
 
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return success Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) external returns (bool success);
 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return remaining Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
-contract MJWToken {
-    //three optional rules
+contract SafeMath {
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+      c = a + b;
+      require(c >= a);
+    }
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+      require(b <= a); c = a - b; } 
+    
+    function safeMul(uint a, uint b) public pure returns (uint c) { 
+      c = a * b; 
+      require(a == 0 || c / a == b); } 
+    
+    function safeDiv(uint a, uint b) public pure returns (uint c) { 
+      require(b > 0);
+      c = a / b;
+    }
+}
+
+contract MJWToken is Token, SafeMath {
+    //three optional items
     string public name;
     string public symbol;
     uint8 public decimals;
@@ -50,5 +69,53 @@ contract MJWToken {
         name = "MJWToken";
         symbol = "MJW";
         decimals = 18;
+        _totalSupply = 100000000000000000000000000;
+        
+        balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
     }
+  
+  // Six mandatory functions
+
+  //Determines the total number of tokens created minus the balance of the contract owner
+  function totalSupply() public override view returns(uint) {
+    return _totalSupply - balances[address(0)];
+  }
+
+  //Returns the number of tokens that a particular address has
+  function balanceOf(address tokenHolder) public override view returns(uint balance) {
+    return balances[tokenHolder];
+  }
+
+  //Makes sure that the user has the minimum number of tokens required for the transaction
+  function allowance(address tokenHolder, address spender) public override view returns(uint remaining) {
+    return allowed[tokenHolder][spender];
+  }
+
+  //Contract owner can give their approval to the user to collect the required number of tokens from the contract's address
+  function approve (address spender, uint tokens) public override returns(bool success) {
+    allowed[msg.sender][spender] = tokens;
+    emit Approval(msg.sender, spender, tokens);
+    return true;
+  }
+
+  //Contract owner can send a given amount of tokens to another address
+  function transfer(address to, uint tokens) public override returns (bool success) {
+    balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+    balances[to] = safeAdd(balances[to], tokens);
+
+    emit Transfer(msg.sender, to, tokens);
+    return true;
+  }
+
+  //This function helps to automate transfers to and from specfic accounts
+  function transferFrom (address from, address to, uint tokens) public override
+	returns (bool success) {
+    balances[from] = safeSub(balances[from], tokens);
+    allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+    balances[to] = safeAdd(balances[to], tokens);
+        
+	emit Transfer(from, to, tokens);
+    return true;
+  }
 }
